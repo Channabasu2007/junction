29c9b583ec0c2fd5b699999a86e85e8ca77d69a1
channabasu2007 <channabasumathad7@gmail.com>
2025-12-27 16:58:59 +0530

    chore: upgrade next.js for security fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   eslint: {
-    // Ignored bcoz file name clashes
-    ignoreDuringBuilds: true,
-  },
+  
   images: {
     remotePatterns: [
       {
